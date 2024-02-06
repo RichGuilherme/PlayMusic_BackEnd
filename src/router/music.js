edee8076f14080f6499c01b78a097cae6effbf1f
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-02-06 20:06:20 -0300

    feat: rota delete

--- src/router/music.js
+++ src/router/music.js
@@ -7,5 +7,6 @@ import musicControllers from "../controllers/musicControllers.js"
 router.post('/create',  uploadAudio.single("music"),  musicControllers.create);
 router.get('/getMusic',  musicControllers.getMusic)
 router.get('/getMusics',  musicControllers.getMusics)
+router.delete('/delete',  musicControllers.deleteMusic)
 
 export default router
