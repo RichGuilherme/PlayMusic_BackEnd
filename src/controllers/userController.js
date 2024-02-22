ce6954fb602a2cceeb11c84c8dffc349e26f7f7e
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-02-21 22:19:55 -0300

    feat: ajuste no json para incluir a url da img do perfil

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,8 @@ import { User } from "../model/User.js"
 class userController {
    getDataUser = async (request, response) => {
        const user = await User.findById(request.user._id)
-       const {username, email, id} = user
-       response.status(200).json({username, email, id})
+       const {username, email, id, imagProfile} = user
+       response.status(200).json({username, email, id, imagProfile})
    }
 
     // Pegar a descrição da playlist
