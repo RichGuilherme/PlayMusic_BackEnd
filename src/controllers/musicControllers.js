f5af8f65ea7cdcd2c888049796889d218e575986
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-02-21 22:24:59 -0300

    feat: rota delete music

--- src/controllers/musicControllers.js
+++ src/controllers/musicControllers.js
@@ -1,14 +1,15 @@
 import { Music } from "../model/Songs.js";
 import { User } from "../model/User.js";
 import { parseBuffer } from 'music-metadata';
-import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { ref, getDownloadURL, uploadBytesResumable, deleteObject } from "firebase/storage";
 import storage from "../config/firebase.js";
 
 class MusicControllers {
     create = async (request, response) => {
         const user = await User.findById(request.user._id)
 
-        // if (playList.songs.length >= 10) {
+        // const maxMusic = 10
+        // if (playList.songs.length >= maxMusic) {
         //     return response.status(403).json("Limite de música alcançado")
         // }
 
@@ -22,7 +23,7 @@ class MusicControllers {
         const metadata = {
             contentType: request.file.mimetype,
         }
-      
+
         const snapshot = await uploadBytesResumable(storageRef, request.file.buffer, metadata)
         const downloadURL = await getDownloadURL(snapshot.ref)
 
@@ -55,17 +56,45 @@ class MusicControllers {
 
     getMusic = async (request, response) => {
         const music = await Music.findById(request.params.musicId)
-        
 
         response.status(200).json({ music })
     }
 
     deleteMusic = async (request, response) => {
-        const music = await Music.findByIdAndDelete(request.query.musicId)
+        const musicId = request.query.musicId
+        const music = await Music.findById(musicId)
+        const userId = request.user._id;
+        const musicIdToDelete = request.query.musicId;
+
+        User.findByIdAndUpdate(userId, {
+            $pull: { musicList: musicIdToDelete }
+        }, { new: true })
+            .then(updatedUser => {
+                console.log('Música deletada com sucesso:', updatedUser);
+            })
+            .catch(err => {
+                console.error('Erro ao deletar música da lista:', err);
+            });
+
+        if (!music) {
+            return response.status(404).send("Música não encontrada")
+        }
+
+        await Music.findByIdAndDelete(musicId)
+
+
+        const storageRef = ref(storage, music.storage_url)
+        await deleteObject(storageRef)
+
+
+        if (music.thumbnail) {
+            const thumbnailRef = ref(storage, music.thumbnail)
+            await deleteObject(thumbnailRef)
+        }
 
         response.status(200).send("Música deletada com sucesso")
     }
-    
+
     getMusics = async (request, response) => {
         const user = await User.findById(request.user._id)
         const musics = await Music.find({ user_id: user._id })
