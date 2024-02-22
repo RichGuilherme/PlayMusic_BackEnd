24ea5341e77466d23930028063c18718fc3cbb7e
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-02-21 21:40:33 -0300

    refactor: ajuster para melhorar o retorno nas authenticações

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,6 +8,7 @@ class UserController {
 
         const hashedPassword = await hashPassword(password)
 
+
         await User.create({ username, password: hashedPassword, email })
             .then(user => {
 
@@ -22,11 +23,10 @@ class UserController {
             })
             .catch(err => {
                 if (err.code === 11000) {
-                    console.error('Erro: Username ou email já existem.');
+                    response.status(401).json({ error: 'Username ou email já existem.' })
                 } else {
-                    console.error('Erro ao criar usuário');
+                    response.status(500).json('Erro ao criar usuário')
                 }
-                response.status(500).json({ error: 'Erro ao criar usuário' })
             })
     }
 
@@ -34,13 +34,10 @@ class UserController {
         const { password, email } = request.body
         const user = await User.findOne({ email })
 
-        if (!user) {
-            return response.status(401).json({ error: 'Email invalido' })
-        }
-
         const passwordsMatch = await comparePasswords(password, user.password)
-        if (!passwordsMatch) {
-            return response.status(401).json({ error: 'Password errado' })
+
+        if (!user || !passwordsMatch) {
+            return response.status(401).json({ error: 'Email ou senha invalida!' })
         }
 
         const token = generateToken(user)
@@ -50,10 +47,23 @@ class UserController {
         })
 
 
-        const { username, id } = user
-        response.status(200).json({ username, email, id })
+        
+        response.status(200).json(user)
     }
 
+
+    googleAuth20 = async (request, response) => {
+        const user = request.user.id
+        const token = generateToken(user)
+        response.cookie("token", token, {
+            maxAge: 300000,
+            secure: true
+        })
+
+
+        const { username, email, id } = user
+        response.status(200).json({ username, email, id })
+    }
 }
 
 export default new UserController
