3bbc6bfa035c86651cbcb6507fab9fa9757ccb7c
RichGuilherme <richard.guilhermeAS@hotmail.com>
2024-03-07 17:49:58 -0300

    refactor:  excluir a rota com a duração da música

--- src/router/music.router.js
+++ src/router/music.router.js
@@ -5,9 +5,8 @@ import {uploadAudio} from "../middleware/multer.js"
 import musicControllers from "../controllers/music.controllers.js"
 
 router.post('/create/:id',  uploadAudio.single("music"),  musicControllers.create);
-router.get('/getMusic/:id',  musicControllers.getMusic)
-router.get('/getMusics/:id',  musicControllers.getMusics)
-router.get('/TimeMusics/:id',  musicControllers.durationTimeMusics)
+router.get('/music/:id',  musicControllers.getMusic)
+router.get('/musics/:id',  musicControllers.getMusics)
 router.delete('/delete',  musicControllers.deleteMusic)
 
 export default router
